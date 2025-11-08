26ff63a29a6aad052e5e91aded2abaae83e47c9f
Neon17 <neon.neupane.9@gmail.com>
2025-11-08 21:50:15 +0545

    fix: eligible_to_donate removed from global scope of donor to ensure donors appear

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -56,7 +56,7 @@ class Donor extends Model
         static::addGlobalScope(('active'), function (Builder $builder) {
             $builder->whereNotNull('last_donated_date');
             $builder->where('last_donated_date', '<=', now()->subDays(90)->toDateString());
-            $builder->where('eligible_to_donate', true);
+            // $builder->where('eligible_to_donate', true);
         });
     }
 
